25eecebc97898eef8cad52fba2f220883980d577
Rashed Arman <rashedarman21@gmail.com>
2023-02-12 22:56:13 +0600

    feat: Connect App and redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,27 @@
 import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store/configureStore';
 
 import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider
-      theme={{
-        fontFamily: 'Montserrat, sans-serif',
-        headings: {
-          fontFamily: 'Poppins, sans-serif',
-        },
-      }}
-      withNormalizeCSS
-      withGlobalStyles
-    >
-      <App />
-    </MantineProvider>
+    <Provider store={store}>
+      <MantineProvider
+        theme={{
+          fontFamily: 'Montserrat, sans-serif',
+          headings: {
+            fontFamily: 'Poppins, sans-serif',
+          },
+        }}
+        withNormalizeCSS
+        withGlobalStyles
+      >
+        <App />
+      </MantineProvider>
+    </Provider>
   </React.StrictMode>,
 );
