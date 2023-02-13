6f5029849615fd1a90914f92ed31ef182ff6cc1d
Rashed Arman <rashedarman21@gmail.com>
2023-02-13 21:39:09 +0600

    feat: Render list of coins

--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,5 +1,24 @@
+import { Text } from '@mantine/core';
+import { useSelector } from 'react-redux';
+import PageWrapper from '../components/PageWrapper';
+import { fetchCoins } from '../store/coins/coinsSlice';
+import { RootState, useAppDispatch } from '../store/configureStore';
+
 function CoinsPage() {
-  return null;
+  const dispatch = useAppDispatch();
+
+  const { coins } = useSelector((state: RootState) => state.coins);
+  if (!coins.length) {
+    dispatch(fetchCoins());
+  }
+
+  return (
+    <PageWrapper>
+      {coins.map((coin) => (
+        <Text key={coin.id}>{coin.name}</Text>
+      ))}
+    </PageWrapper>
+  );
 }
 
 export default CoinsPage;
