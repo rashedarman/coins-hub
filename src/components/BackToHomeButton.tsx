a8582e6de63b4ef064bc79e4677ae89a82f6af60
Rashed Arman <rashedarman21@gmail.com>
2023-02-14 15:43:25 +0600

    refactor: Accept style props in BackToHomeButton

--- src/components/BackToHomeButton.tsx
+++ src/components/BackToHomeButton.tsx
@@ -1,13 +1,15 @@
 /// <reference types="vite-plugin-svgr/client" />
-import { Button } from '@mantine/core';
+import { Button, ButtonProps } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
 import { ReactComponent as IconHome } from '../assets/IconHome.svg';
 
-function BackToHomeButton() {
+type Props = ButtonProps
+
+function BackToHomeButton(props: Props) {
   const navigate = useNavigate();
 
   return (
-    <Button onClick={() => navigate('/')} leftIcon={<IconHome />}>
+    <Button onClick={() => navigate('/')} leftIcon={<IconHome />} {...props}>
       Back to Home
     </Button>
   );
