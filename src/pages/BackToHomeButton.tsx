002411bc2b1c65100049adbd86a8a7d12a25870c
Rashed Arman <rashedarman21@gmail.com>
2023-02-14 11:40:40 +0600

    feat: Navigate to home

--- src/pages/BackToHomeButton.tsx
+++ src/pages/BackToHomeButton.tsx
@@ -1,9 +1,16 @@
 /// <reference types="vite-plugin-svgr/client" />
 import { Button } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
 import { ReactComponent as IconHome } from '../assets/IconHome.svg';
 
 function BackToHomeButton() {
-  return <Button leftIcon={<IconHome />}>Back to Home</Button>;
+  const navigate = useNavigate();
+
+  return (
+    <Button onClick={() => navigate('/')} leftIcon={<IconHome />}>
+      Back to Home
+    </Button>
+  );
 }
 
 export default BackToHomeButton;
