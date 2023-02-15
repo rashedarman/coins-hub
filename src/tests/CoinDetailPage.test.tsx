e071c714426ae6c0c841a441c4f7885f5bed6a09
Rashed Arman <rashedarman21@gmail.com>
2023-02-15 06:59:57 +0600

    test: Mock coindId for detail page

--- src/tests/CoinDetailPage.test.tsx
+++ src/tests/CoinDetailPage.test.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import CoinDetailsPage from '../pages/CoinDetail';
 import store from '../store/configureStore';
 
+// Mock the useParams hook
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({ coinId: 'bitcoin' }),
+}));
+
 describe('Testing CoinsListTable component', () => {
   it('Renders CoinsListTable component correctly', () => {
     const { container } = render(
