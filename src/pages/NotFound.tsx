97f36573c49cb63d9a77ed514ba89196ea37e7bc
Rashed Arman <rashedarman21@gmail.com>
2023-02-17 18:36:20 +0600

    style: Adjust 404 page for dark mode

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,10 @@
-import { Flex, Title } from '@mantine/core';
-import BackToHomeButton from '../components/BackToHomeButton';
+import { Button, Flex, Title } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+import { ReactComponent as IconHome } from '../assets/IconHome.svg';
 
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
     <Flex
       direction="column"
@@ -10,8 +13,10 @@ function NotFound() {
       gap="2rem"
       sx={{ marginTop: '12rem' }}
     >
-      <Title color="gray">Oops! You seem to be lost.</Title>
-      <BackToHomeButton variant="gradient" />
+      <Title>Oops! You seem to be lost.</Title>
+      <Button onClick={() => navigate('/')} leftIcon={<IconHome />}>
+        Back To Home
+      </Button>
     </Flex>
   );
 }
