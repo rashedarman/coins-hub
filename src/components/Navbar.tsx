fa347f183314884edeb351c95d4775d301fc20e0
Rashed Arman <rashedarman21@gmail.com>
2023-02-17 11:34:42 +0600

    style: Simplify Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,41 +1,12 @@
-import {
-  Anchor, Button, Container, Flex, Paper,
-} from '@mantine/core';
-import { Link, useLocation } from 'react-router-dom';
+import { Anchor, Container, Paper } from '@mantine/core';
 
 function Navbar() {
-  const location = useLocation();
-
   return (
     <Paper radius={0} shadow="sm" p="sm">
       <Container size="xl">
-        <Flex justify="space-between">
-          <Anchor
-            href="/"
-            variant="text"
-            c="blue"
-            fw={700}
-            style={{ fontSize: '1.75rem' }}
-          >
-            CoinsHub
-          </Anchor>
-          <Flex align="center" gap="sm">
-            <Button
-              variant={location.pathname === '/' ? 'light' : 'subtle'}
-              component={Link}
-              to="/"
-            >
-              Coins
-            </Button>
-            <Button
-              variant={location.pathname === '/news' ? 'light' : 'subtle'}
-              component={Link}
-              to="/news"
-            >
-              News
-            </Button>
-          </Flex>
-        </Flex>
+        <Anchor href="/" variant="text" c="blue" fw={700} fz="xl">
+          CoinsHub
+        </Anchor>
       </Container>
     </Paper>
   );
