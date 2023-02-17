b5ee7681cab0d4e472d649f750f7edd95e95df58
Rashed Arman <rashedarman21@gmail.com>
2023-02-17 15:31:37 +0600

    style: Match BackToHomeButton with wireframe

--- src/components/BackToHomeButton.tsx
+++ src/components/BackToHomeButton.tsx
@@ -1,16 +1,23 @@
 /// <reference types="vite-plugin-svgr/client" />
 import { Button, ButtonProps } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
-import { ReactComponent as IconHome } from '../assets/IconHome.svg';
+import { ReactComponent as IconChevronLeft } from '../assets/IconChevronLeft.svg';
 
 type Props = ButtonProps;
 
 function BackToHomeButton(props: Props) {
   const navigate = useNavigate();
-
   return (
-    <Button onClick={() => navigate('/')} leftIcon={<IconHome />} {...props}>
-      Back to Home
+    <Button
+      onClick={() => navigate('/')}
+      compact
+      variant="subtle"
+      color="dark"
+      p="0"
+      sx={{ '&:hover': { backgroundColor: 'transparent' } }}
+      {...props}
+    >
+      <IconChevronLeft />
     </Button>
   );
 }
